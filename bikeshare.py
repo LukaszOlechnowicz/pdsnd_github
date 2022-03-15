cd3f0186b5a18daa955be43ab6fe24de1bc0fd60
Olechnowicz Lukasz <lukasz.olechnowicz@volvo.com>
2022-03-15 23:47:38 +0100

    fix: 6 rows instead of 5 of raw data presented in bikshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -193,8 +193,8 @@ def raw_data_examples(df):
     while True:
         examples_input = input('\nWould you like to see raw data? Enter yes or no.\n').lower()
         if examples_input in 'yes':
-            print(df.iloc[range(i,i+5)])
-            i+=5
+            print(df.iloc[range(i,i+6)])
+            i+=6
         else:
             break            
     
